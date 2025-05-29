26edf481266cc15de97f1898830bf3fe1376f407
SinghAyush07 <aysingh0016@gmail.com>
2025-05-29 11:53:22 +0530

    week-2-soln

---
+++ week-2-solution/week-2-asyncjs/medium/1-file-cleaner.js
@@ -0,0 +1,13 @@
+
+const fs = require('fs');
+
+fs.readFile('a.txt','utf-8',(err,content) => {
+    if (err)
+        console.log(err);
+    content= content.replaceAll(/ +/g,' ');
+    console.log(content);
+    fs.writeFile('a.txt',content,(err) => {
+        if (err)
+            console.log(err);
+    })
+});
